91d3f4492d482bbdfd427ed3d4a13b73f468c950
Megrez <huangtianquan@gameley.cn>
2022-09-20 19:29:05 +0800

    echo some message

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@ mv githooks-main/init.md ./
 mv githooks-main/init_git_hooks.sh ./
 rm -rf githooks-main*
 
+echo "===== move engine config file ====="
+
 engine_conf=$1
 perl -pi -e "s|engine_conf|$engine_conf|" init_git_hooks.sh
 mv $engine_conf .githooks
